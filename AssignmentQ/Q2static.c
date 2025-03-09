50105d5821cfb81e390c6f70b43219f065803106
aseeriman <aseeriman18@gmail.com>
2025-03-09 16:51:29 +0500

    Execution time for static

--- AssignmentQ/Q2static.c
+++ AssignmentQ/Q2static.c
@@ -18,3 +18,29 @@ long long compute_sum_static(int num_threads) {
 
     return sum;
 }
+
+int main() {
+    // Initialize array
+    for (long long i = 0; i < N; i++) {
+        arr[i] = i;
+    }
+
+    int num_threads = 4;
+    long long total_time = 0;
+
+    
+    for (int i = 0; i < 10; i++) {
+        double start = omp_get_wtime();
+        long long sum = compute_sum_static(num_threads);
+        double end = omp_get_wtime();
+        
+        long long exec_time = (end - start) * 1000;
+        total_time += exec_time;
+        
+        printf("Run %d Execution Time: %lld ms\n", i + 1, exec_time);
+    }
+
+    printf("Average Execution Time (Static Scheduling): %.2f ms\n", total_time / 10.0);
+
+    return 0;
+}
